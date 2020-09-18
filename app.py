b8b985299ddd2164fbc1b0729819e60f7a8d04f5
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-09-17 19:48:50 -0700

    set up routes

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.router('/')
+@app.route('/')
 def hello_world():
 	return "Hello World!"
 
+@app.route('/hithere')
+def hi_there_everyone():
+	return "I just hit /hithere"
+
+@app.route('/bye')
+def bye():
+	#Prepare a response for the request that came to /bye
+	c = 2*534
+	s = str(c)
+	# c = 1/0
+	return "bye"
+
 if __name__ == "__main__":
-	app.run()
+	# app.run(host="127.0.0.1", port=80)   for Production
+	 app.run(debug=True)
