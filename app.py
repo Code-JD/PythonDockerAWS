61b7b16bdb53912226a0acce3123f2885ad9e0fa
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-09-19 17:02:22 -0700

    setup api routes for Get and Post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,14 +9,38 @@ def hello_world():
 def hi_there_everyone():
 	return "I just hit /hithere"
 
+@app.route('/add_two_nums', methods=["POST"])
+def add_two_nums():
+	#Get x,y from the posted date
+	dateDict = request.get_json()
+	return jsonify(dataDict)
+	#Add z=x+y
+
+	#Prepare a JSON, "z":z
+
+	#Return jsonify(map_prepared)
+
 @app.route('/bye')
 def bye():
 	#Prepare a response for the request that came to /bye
-	c = 2*534
-	s = str(c)
-	# c = 1/0
-	return "bye"
+	age = 2*5
+	
+	retJson = {
+		'Name': 'Jonny',
+		'Age': age,
+		"phones": [
+			{
+				"phoneName": "Galaxy S10+",
+				"phoneNumber": 5086236
+			},
+			{
+				"phoneName": "Iphone10",
+				"phoneNumber": 5313306
+			}
+		]
+	}
+	return jsonify(retJson)
 
 if __name__ == "__main__":
 	# app.run(host="127.0.0.1", port=80)   for Production
-	 app.run(debug=True)
+	 app.run(debug=True),
